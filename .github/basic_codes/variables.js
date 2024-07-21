6f5ac93e5de97bda6d8158e7700c771dddd3a129
Riddhi Singh <2004singh.riddhi@gmail.com>
2024-07-21 05:57:54 +0000

    variables

---
+++ .github/basic_codes/variables.js
@@ -0,0 +1,10 @@
+const studentId = 201
+let studentName = "Riddhi"
+let studentLname = "Singh"
+let studentCourse = "Computer Science"
+let studentAge = 20
+let studentHobby
+
+
+console.table([studentId, studentName, studentLname, studentCourse, studentAge, studentHobby]);
+//using var keyword makes the program to suffer from block scope{}
