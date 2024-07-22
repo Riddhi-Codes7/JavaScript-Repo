7f619b7867f3d960ade721dbc7ad603b6363ba84
Riddhi Singh <2004singh.riddhi@gmail.com>
2024-07-22 07:02:26 +0000

    dataTypes_interview

---
+++ .github/basic_codes/dataTypes_interview.js
@@ -0,0 +1,29 @@
+/* Primitive datatypes are called by value and Non-Primitive datatypes are called by reference
+
+*/
+console.table(["Number","BigInt", "String", "Boolean", "Null", "Undefined", "Symbol" ]);
+console.table(["Object" , "Array", "Functions"])
+
+//*********  Non-Primitive ***********
+//Array and object
+let FoodItems = ["CheeseCake", "MudPie", "Pastries", "Tiramisu"]
+let myObj= {
+    shopName : "Baskins and Robbins",
+    location : "Paris",
+}
+//Declaring Function as variables
+
+let myFunction = function(){
+    console.log("Hellowww Cutiess :-)")
+}
+
+/*
+Datatypes       return_types
+
+String          string
+Number          number
+Undefined       undefined
+Null            object
+Boolean         boolean
+Function        function
+*/
