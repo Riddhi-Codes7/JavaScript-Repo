268a90282e74e736c1f0e4a0d4575be0c560e344
Riddhi Singh <2004singh.riddhi@gmail.com>
2024-07-23 05:53:20 +0000

    dataTypes_interview

--- .github/basic_codes/dataTypes_interview.js
+++ .github/basic_codes/dataTypes_interview.js
@@ -27,3 +27,45 @@ Null            object
 Boolean         boolean
 Function        function
 */
+
+
+____________________________________________________________
+
+//MEMORIES : 1. STACK (Primitive)  2. HEAP (Non-primitive)
+
+/*==> 1.STACK (copy)
+let bankaccount = "AxisBank";
+let accountName =  bankaccount;
+
+{Interesting point} accountName  = "HDFCbank"
+
+console.log("bankaccount");
+console.log("accountName");
+
+{OUTPUT:
+bankaccount: Axisbank
+accountName: HDFCbank
+}
+
+//when the new value is assigned to accountName, the copy of bankaccount is created and the new value is 
+stored, hence the original value of bankaccount will remainn the same. Updates in the copy value
+*/
+
+/* ==> 2. HEAP (reference)
+let student1 = {
+    name: "Riddhi",
+    age: 20,
+}
+let student2 = student1
+student2.name: "Nishtha"
+
+console.log(student1.name)
+console.log(student2.name)
+
+{OUTPUT:
+Nishtha
+Nishtha
+
+//Heap memory works with the refernce, Updates in the Original value
+}
+*/
